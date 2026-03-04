01dc7e4be5fb98fbf4d4c72a5f49047f5e28996e
Guillermo <guillermocumbrerasantana@alumno.ieselrincon.es>
2026-03-04 15:48:29 +0000

    Estructura incial: Clase Tarea

--- src/gestortareas/Tarea.java
+++ src/gestortareas/Tarea.java
@@ -5,9 +5,37 @@
 package gestortareas;
 
 /**
- *
- * @author guill
+ * Clase que representa una tarea individual en el sistema.
+ * @author Guillermo
  */
 public class Tarea {
-    
+    private String descripcion;
+    private boolean completada;
+
+    /**
+     * Constructor para crear una nueva tarea.
+     * @param descripcion El texto descriptivo de la tarea.
+     */
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        String estado = completada ? "[Completada]" : "[Pendiente]";
+        return estado + " " + descripcion;
+    }
 }
