7b93dffde50c2b6528eef548b667ba2adebc90d1
Guillermo <guillermocumbrerasantana@alumno.ieselrincon.es>
2026-03-04 16:03:05 +0000

    Estructura incial: Clase Tarea

---
+++ src/gestortareas/GestorTarea.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestortareas;
+
+import java.util.ArrayList;
+
+/**
+ * Lógica de negocio para gestionar la lista de tareas.
+ * @author Guillermo y Judith
+ */
+public class GestorTarea {
+    private ArrayList<Tarea> listaTareas;
+
+    public GestorTarea() {
+        this.listaTareas = new ArrayList<>();
+    }
+}
