c78ac026d007194b98af7db520ea359da02191d0
Judith <judithsantanasanz@alumno.ieselrincon.es>
2026-03-04 16:08:32 +0000

    Añadida funcionalidad de listado

--- src/gestortareas/GestorTarea.java
+++ src/gestortareas/GestorTarea.java
@@ -8,12 +8,28 @@ import java.util.ArrayList;
 
 /**
  * Lógica de negocio para gestionar la lista de tareas.
+ *
  * @author Guillermo y Judith
  */
 public class GestorTarea {
+
     private ArrayList<Tarea> listaTareas;
 
     public GestorTarea() {
         this.listaTareas = new ArrayList<>();
+    }
+
+    /**
+     * * Muestra todas las tareas por consola (Implementado por Judith).
+     */
+    public void mostrarTareas() {
+        if (listaTareas.isEmpty()) {
+            System.out.println("La lista está vacía.");
+        } else {
+            System.out.println("\n--- LISTA DE TAREAS ---");
+            for (int i = 0; i < listaTareas.size(); i++) {
+                System.out.println((i + 1) + ". " + listaTareas.get(i).toString());
+            }
+        }
     }
 }
